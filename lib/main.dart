733945d031710d02de5dfbe55c5007b7655b68d0
John6150 <olalekejohnny@gmail.com>
2025-05-05 17:14:02 +0100

    image upload and conversion

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 // import 'dart:convert';
 import 'package:is_first_run/is_first_run.dart';
 
@@ -14,11 +18,16 @@ import 'package:onboardingscreen/onboardingScreen.dart';
 import 'package:onboardingscreen/routes.dart';
 import 'package:onboardingscreen/variables.dart';
 import 'package:onboardingscreen/variables.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
   runApp(ProviderScope(child: const MyApp()));
 }
+
+File? dpImg;
+// File? compressedDpImg;
+Uint8List? res;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -93,10 +102,52 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
                             ),
                           ),
                           onPressed: () async {
-                            Map<String, dynamic> prod = await result();
-                            setState(() {
-                              count = prod['data'];
-                            });
+                            // Map<String, dynamic> prod = await result();
+                            // print('this is prod: $prod');
+                            // if (prod != null) {
+                            //   setState(() {
+                            //     // count = prod['data'];
+                            //   });
+                            // }
+                            ImagePicker picker = ImagePicker();
+                            final XFile? img = await picker.pickImage(
+                              source: ImageSource.gallery,
+                            );
+                            if (img != null) {
+                              // setState(() {
+                              //   dpImg = File(img.path);
+                              // });
+                              
+                              Uint8List ress = await testCompressFile(img);
+                              setState(() {
+                                res = ress;
+                              });
+
+                              final dir = await getTemporaryDirectory();
+
+                              File finalImg =
+                                  await File('${dir.path}/images.jpg').create();
+
+                              await finalImg
+                                  .writeAsString(img.path)
+                                  .then(
+                                    (re) => setState(() {
+                                      dpImg = re;
+                                    }),
+                                  );
+
+                              // print(finalImg);
+
+                              // print;
+                              // final Map<String, dynamic> result =
+                              //     await postRequest(
+                              //       'John',
+                              //       '100',
+                              //       'Flutter',
+                              //       File(img.path),
+                              //     );
+                              // print(img.runtimeType);
+                            }
                           },
                           child: Text(
                             'Click here',
@@ -104,58 +155,66 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
                           ),
                         ),
                         SizedBox(height: 50),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height * 0.5,
 
-                          child: ListView.builder(
-                            itemCount: count.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                leading: CachedNetworkImage(
-                                  imageBuilder:
-                                      (context, imageProvider) => Container(
-                                        decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                            image: imageProvider,
-                                            fit: BoxFit.cover,
-                                            colorFilter: ColorFilter.mode(
-                                              Colors.red,
-                                              BlendMode.colorBurn,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                  errorWidget:
-                                      (context, url, error) =>
-                                          Icon(Icons.error),
-                                  imageUrl: '${count[index]['images'][0]}',
-                                  // imageUrl: 'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png',
-                                  height: 30,
-                                  width: 30,
-                                  progressIndicatorBuilder: (
-                                    context,
-                                    url,
-                                    progress,
-                                  ) {
-                                    return CircularProgressIndicator(
-                                      value: progress.progress,
-                                    );
-                                  },
-                                ),
+                        // Container(
+                        //   width: MediaQuery.of(context).size.width,
+                        //   height: MediaQuery.of(context).size.height * 0.5,
 
-                                // Image(image: CachedNetworkImageProvider(count[index]['images'][0],)),
+                        //   child: ListView.builder(
+                        //     itemCount: count.length,
+                        //     itemBuilder: (context, index) {
+                        //       return ListTile(
+                        //         leading: CachedNetworkImage(
+                        //           imageBuilder:
+                        //               (context, imageProvider) => Container(
+                        //                 decoration: BoxDecoration(
+                        //                   image: DecorationImage(
+                        //                     image: imageProvider,
+                        //                     fit: BoxFit.cover,
+                        //                     colorFilter: ColorFilter.mode(
+                        //                       Colors.red,
+                        //                       BlendMode.colorBurn,
+                        //                     ),
+                        //                   ),
+                        //                 ),
+                        //               ),
+                        //           errorWidget:
+                        //               (context, url, error) =>
+                        //                   Icon(Icons.error),
+                        //           imageUrl: '${count[index]['images'][0]}',
+                        //           // imageUrl: 'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png',
+                        //           height: 30,
+                        //           width: 30,
+                        //           progressIndicatorBuilder: (
+                        //             context,
+                        //             url,
+                        //             progress,
+                        //           ) {
+                        //             return CircularProgressIndicator(
+                        //               value: progress.progress,
+                        //             );
+                        //           },
+                        //         ),
 
-                                //  Image.network(
-                                //   count[index]['images'][0],
-                                // ),
-                                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                                title: Text(count[index]['title']),
-                                subtitle: Text(count[index]['brand'] ?? ''),
-                              );
-                            },
-                          ),
-                        ),
+                        //         // Image(image: CachedNetworkImageProvider(count[index]['images'][0],)),
+
+                        //         //  Image.network(
+                        //         //   count[index]['images'][0],
+                        //         // ),
+                        //         trailing: Icon(Icons.arrow_forward_ios_rounded),
+                        //         title: Text(count[index]['title']),
+                        //         subtitle: Text(count[index]['brand'] ?? ''),
+                        //       );
+                        //     },
+                        //   ),
+                        // ),
+                        SizedBox(height: 20),
+                        dpImg == null
+                            ? SizedBox()
+                            : Image(image: FileImage(dpImg!)),
+                        // Text(dpImg!.readAsBytes().toString()),
+                        res == null ? SizedBox() : Image.memory(res!),
+                        // Text('${res!.length.toString()} bytes '),
                       ],
                     ),
                   ),
@@ -184,13 +243,14 @@ Future<Map<String, dynamic>> result() async {
   try {
     response = await _axios.get('/products');
     print(response);
-    Map<String, dynamic> res = jsonDecode(response.data);
+    // Map<String, dynamic> res = {};
+    Map<String, dynamic> res = jsonDecode(response.toString());
+    print(res);
+    //
     for (var element in res['products']) {
-      // setstate({});
       count.add(element);
     }
-    print(count);
-    print(res);
+    // print(count);
     return {'data': res['products']};
   } on DioException catch (e) {
     return {'status': 'failed', 'message': e};
@@ -208,7 +268,7 @@ Dio axios([token]) {
   dio.options.connectTimeout = (Duration(seconds: 5));
   dio.options.sendTimeout = Duration(seconds: 5);
 
-  dio.options.extra = {'id': ''};
+  // dio.options.extra = {'id': ''};
   dio.interceptors.add(
     InterceptorsWrapper(
       onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
@@ -259,3 +319,39 @@ Dio axios([token]) {
 
   return dio;
 }
+
+Future<Map<String, dynamic>> postRequest(
+  name,
+  age,
+  studentsClass,
+  File image,
+) async {
+  final dio = axios();
+  try {
+    FormData formData = FormData.fromMap({
+      'post': '',
+      'id': '',
+      'description': '',
+      'image': await MultipartFile.fromFile(image.path),
+    });
+    dio.post('', data: formData);
+    return {};
+  } catch (e) {
+    return {};
+  }
+}
+
+Future<Uint8List> testCompressFile(img) async {
+  var result = await FlutterImageCompress.compressWithFile(
+    img.path,
+    minWidth: 500,
+    minHeight: 500,
+    quality: 100,
+    // rotate: 90,
+  );
+  // print(file.lengthSync());
+  print(result!.length);
+
+  return result;
+}
+// 38957
